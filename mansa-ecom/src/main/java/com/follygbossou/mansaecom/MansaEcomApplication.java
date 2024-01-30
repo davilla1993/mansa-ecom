904d12427dc761268128a77a7cfafec22a5acd78
davilla1993 <carlogbossou93@gmail.com>
2024-01-30 21:01:54 +0000

    initial commit

---
+++ mansa-ecom/src/main/java/com/follygbossou/mansaecom/MansaEcomApplication.java
@@ -0,0 +1,13 @@
+package com.follygbossou.mansaecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MansaEcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MansaEcomApplication.class, args);
+	}
+
+}
---
+++ mansa-ecom/src/test/java/com/follygbossou/mansaecom/MansaEcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.follygbossou.mansaecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MansaEcomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
